ad5bd60ddebf7bbc2b452101b681d1c979c226bf
Ajith <ajithpkumar231@gmail.com>
2023-04-07 17:04:25 +0530

    adding hackathons data

--- app.py
+++ app.py
@@ -2,6 +2,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+hackathons={
+    "GHW: API Week":{
+    "start_date":"2023-04-03 12:00:00",
+    "end_date":"2023-04-10 12:00:00",
+    "location":"Everywhere, Online",
+    "type":"Digital Only"
+    },
+    "Bitcamp":{
+    "start_date":"2023-04-07 12:00:00",
+    "end_date":"2023-04-9 12:00:00",
+    "location":"College Park, Maryland",
+    "type":"In-Person Only"
+    }
+}
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('./gethackathons', method=['GET'])
+def gethackathons():
+    return hackathons
